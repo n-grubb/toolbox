c0ef4fc3d1a8a9325070fc9741cdf031dab72e22
Noah Grubb <noah@blackfynn.com>
2021-02-11 12:29:27 -0500

    Config Vite to properly build package. Fix print styles.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+// Global Styles
+import './assets/styles/main.css'
+// Components
+import ToolboxIcon from './components/ToolboxIcon/ToolboxIcon.vue'
+import VisuallyHidden from './components/VisuallyHidden/VisuallyHidden.vue'
 // Icon Library
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-// Components
-import ToolboxIcon from './components/ToolboxIcon/ToolboxIcon.vue'
-import VisuallyHidden from './components/VisuallyHidden/VisuallyHidden.vue'
-// Global Styles
-import './assets/styles/main.css'
 
 // Setup Font Awesome to use all icons
 library.add(fas)
 
-export const components = {
+const components = {
   ToolboxIcon,
   VisuallyHidden
 }
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+import vue from '@vitejs/plugin-vue'
+
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'toolbox'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+}
